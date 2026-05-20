d4b1eefb1db3234cf71aa5fbf449661d6d5d7282
Tanveer Alam <ansaritaz78601@gmail.com>
2026-05-20 11:16:25 +0530

    updated icons

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -200,40 +200,25 @@ const socials: SocialLink[] = [
     text: "Github",
     icon: "/icons/github.svg",
     bg: "#f4656b",
-    link: "https://github.com/JavaScript-Mastery-Pro",
+    link: "https://github.com/7AZz",
   },
   {
     id: 2,
-    text: "Platform",
-    icon: "/icons/atom.svg",
-    bg: "#4bcb63",
-    link: "https://jsmastery.com/",
-  },
-  {
-    id: 3,
     text: "Twitter/X",
     icon: "/icons/twitter.svg",
     bg: "#ff866b",
-    link: "https://x.com/jsmasterypro",
+    link: "https://x.com/tanveer___dev",
   },
   {
-    id: 4,
+    id: 3,
     text: "LinkedIn",
     icon: "/icons/linkedin.svg",
     bg: "#05b6f6",
-    link: "https://www.linkedin.com/company/javascriptmastery/posts/?feedView=all",
+    link: "https://www.linkedin.com/in/tanveer-alam-dev/",
   },
 ];
 
-
-
-export {
-  navLinks,
-  navIcons,
-  dockApps,
-  techStack,
-  socials,
-};
+export { navLinks, navIcons, dockApps, techStack, socials };
 
 const WORK_LOCATION: LocationRoot = {
   id: 1,
